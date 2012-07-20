769cf41e919c2e9b13473b4f777a797bf7455d21
gaoquanbing <gaoquanbing@ausxin.com>
2012-07-20 23:30:34 +0800

    update

--- src/Github.java
+++ src/Github.java
@@ -5,8 +5,9 @@ public class Github {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("I like gitbub");
-
+		if(true){
+			System.out.println("I like gitbub");
+		}
 	}
 
 }
