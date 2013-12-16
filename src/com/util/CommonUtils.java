06c63f9aa00c999c2669a845646d1aa42c1f3622
brucegaochina <brucegaochina@gmail.com>
2013-12-16 11:02:09 +0800

    pp

--- src/com/util/CommonUtils.java
+++ src/com/util/CommonUtils.java
@@ -12,11 +12,11 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/** 
- * @ClassName: CommonUtils 
+/**
+ * @ClassName: CommonUtils
  * @Description: TODO
- * @author Bruce 
- * @date Oct 16, 2012 
+ * @author Bruce
+ * @date Oct 16, 2012
  */
 public class CommonUtils {
 	/**
@@ -47,75 +47,75 @@ public class CommonUtils {
 			return true;
 		return false;
 	}
-	
+
 	/**
 	 * 当csv文件的某个列中存在分隔符,把其合并起来
 	 */
-	public static String[] formatCsvRow(String[] line){
-		if(line == null || line.length == 0)
+	public static String[] formatCsvRow(String[] line) {
+		if (line == null || line.length == 0)
 			return null;
-		
+
 		String[] tmp = line;
 		int length = tmp.length;
-		
+
 		List<String> list = new ArrayList<String>();
 
-		for(int i = 0;i < length;i++){
-			
+		for (int i = 0; i < length; i++) {
+
 			StringBuffer buffer = new StringBuffer(tmp[i]);
-			while(!isValidField(buffer.toString())){
+			while (!isValidField(buffer.toString())) {
 				buffer.append(" " + tmp[++i]);
 			}
-			
+
 			list.add(buffer.toString());
 		}
-		
+
 		String[] result = list.toArray(new String[list.size()]);
 		return result;
 	}
-	
-	private static boolean isValidField(String str){
-		if(str == null || "".equals(str.trim()))
+
+	private static boolean isValidField(String str) {
+		if (str == null || "".equals(str.trim()))
 			return true;
-		
+
 		int count = 0;
-		for(int i = 0;i < str.length();i++){
-			if(str.charAt(i) == '"')
-				count ++;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '"')
+				count++;
 		}
-		
-		if(count%2 == 0)
+
+		if (count % 2 == 0)
 			return true;
-		
+
 		return false;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		File file = new File("C:\\Users\\Bruce\\Desktop\\Outlook (6)\\test.txt");
 		BufferedReader br = null;
-		try{
+		try {
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(
 					file), "gb2312"));
-			
+
 			String line = null;
-			while((line = br.readLine()) != null ){
+			while ((line = br.readLine()) != null) {
 				String row[] = line.split(",", -1);
-				
+
 				String n[] = formatCsvRow(row);
-				
+
 				int i = 1;
 
 				System.out.println(n.toString());
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	/** 
+
+	/**
 	 * @Description: 转换日期格式 DD/MM/YYYY -> YYYY-MM-DD
 	 * 
-	 */  
+	 */
 	public static String formatDate(String lang) {
 		String result = "";
 
