f7318247db3f56908c46eb9a31a76354fc6f6643
brucegaochina <brucegaochina@gmail.com>
2013-12-16 10:57:37 +0800

    op

--- src/com/util/LocaleUtils.java
+++ src/com/util/LocaleUtils.java
@@ -4,17 +4,17 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
- * internationalization
+ * internationalization 
  * 
  * @author Bruce
- * 
+ *
  * @Date 2012-8-22
  */
 public class LocaleUtils {
-
-	public static String getLocal(String key) {
+	
+	public static String getLocal(String key){
 		Locale locale = Locale.getDefault();
-		ResourceBundle bundle = ResourceBundle.getBundle("report", locale);
+		ResourceBundle  bundle = ResourceBundle.getBundle("report", locale);
 		return bundle.getString(key);
 	}
 
